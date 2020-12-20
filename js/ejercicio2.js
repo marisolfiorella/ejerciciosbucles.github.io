e016141ea84148439ff86e40591b2ba9bca10347
Marisol Fiorella <marisolfiorellasan@gmail.com>
2020-12-20 13:12:32 -0300

    Agregue enunciado a los ejercicios

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,3 +1,15 @@
+/*Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+●	0-2: Muy deficiente
+●	3-4: Insuficiente
+●	5-6: Suficiente
+●	7: Bien
+●	8-9: Notable
+●	10: Sobresaliente
+
+Si ingreso un numero que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el numero ingresado no es válido mostrar el mensaje “Introduce un número válido”.
+*/
+
 let nota = parseInt(prompt("Ingrese una nota del 1-10"));
 
 switch (nota) {
